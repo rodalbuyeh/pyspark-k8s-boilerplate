d0f760d51734882f871ae07d3340bf52812cd1bd
rodalbuyeh <rod@albellcloud.com>
2021-08-24 10:49:53 -0700

    add default configs for function args

--- src/pyspark_k8s_boilerplate/jobs/cloud_etl.py
+++ src/pyspark_k8s_boilerplate/jobs/cloud_etl.py
@@ -1,16 +1,21 @@
 from pyspark_k8s_boilerplate.utils.pyspark import get_spark_session
 from pyspark_k8s_boilerplate.utils.log import logger
+from pyspark.sql import DataFrame
+from pyspark_k8s_boilerplate.config.handlers import data_cfg
 
 
-def run_cloud_etl(bucket_root: str):
+def run_cloud_etl(bucket_root: str = data_cfg.object_bucket) -> None:
 
-	spark = get_spark_session()
+	spark = get_spark_session("cloud_etl")
 
 	logger.info("Read titanic.csv to spark dataframe")
-	df = spark.read.csv(f"{bucket_root}titanic.csv", header=True)
+	df: DataFrame = spark.read.csv(f"{bucket_root}titanic.csv", header=True)
 
 	logger.info("Summarize and write to object storage as parquet")
 	df.describe().write.parquet(f"{bucket_root}titanic_summary.parquet")
 
 	spark.stop()
 
+
+if __name__ == "__main__":
+	run_cloud_etl()
--- src/pyspark_k8s_boilerplate/jobs/pi.py
+++ src/pyspark_k8s_boilerplate/jobs/pi.py
@@ -3,17 +3,18 @@ from operator import add
 
 from pyspark_k8s_boilerplate.utils.pyspark import get_spark_session
 from pyspark_k8s_boilerplate.utils.log import logger
+from pyspark_k8s_boilerplate.config.handlers import data_cfg
 
 
-def spark_pi(partitions: int = 2) -> None:
+def spark_pi(partitions: int = data_cfg.pi_partitions) -> None:
 
     spark = get_spark_session("PythonPi")
 
     n = 100000 * partitions
 
     def f(_: int) -> int:
-        x = random() * 2 - 1
-        y = random() * 2 - 1
+        x: float = random() * 2 - 1
+        y: float = random() * 2 - 1
         return 1 if x ** 2 + y ** 2 <= 1 else 0
 
     count = spark.sparkContext.parallelize(range(1, n + 1),
