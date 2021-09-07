df6756eeff2756f967aa88e972b669389ee25791
rodalbuyeh <rod@albellcloud.com>
2021-09-07 14:11:07 -0700

    add todos

--- src/pyspark_k8s_boilerplate/main.py
+++ src/pyspark_k8s_boilerplate/main.py
@@ -77,3 +77,6 @@ if __name__ == "__main__":
 # TODO document, add infra code
 # TODO maybe jupyter on master
 # TODO spark history server
+# TODO fix authentication to not use json keyfile.. or just use volume mount
+# TODO remove lame kubectl commands from makefile
+# TODO try moving user=185 earlier in dockerfile.. or do run-as command
