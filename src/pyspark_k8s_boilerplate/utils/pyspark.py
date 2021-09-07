696db3c5a9d46fefda22df53074c6cbe173d1d10
rodalbuyeh <rod@albellcloud.com>
2021-09-07 10:23:55 -0700

    well at least the param is there

--- src/pyspark_k8s_boilerplate/utils/pyspark.py
+++ src/pyspark_k8s_boilerplate/utils/pyspark.py
@@ -1,6 +1,8 @@
 
 from pyspark_k8s_boilerplate.utils.log import logger
+from pyspark_k8s_boilerplate.config.handlers import cfg
 from pyspark.sql import SparkSession
+from pyspark import SparkConf
 
 
 def get_spark_session(app_name: str) -> SparkSession:
@@ -10,4 +12,8 @@ def get_spark_session(app_name: str) -> SparkSession:
     :return: A spark session
     """
     logger.info("Getting the Spark Session.")
-    return SparkSession.builder.appName(app_name).getOrCreate()
+
+    return SparkSession.builder.config("spark.eventLog.enabled", "true")\
+        .config("spark.eventLog.dir", cfg.log_bucket)\
+        .appName(app_name)\
+        .getOrCreate()
